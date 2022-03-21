6b537bd39e6f7e89ce64a64d66217de6618b6784
Jacob <callaghanjacob@gmail.com>
2022-03-21 16:19:09 +0000

    player and event index.html both ok now

--- controllers/event_controller.py
+++ controllers/event_controller.py
@@ -3,3 +3,8 @@ from flask import Flask, render_template, request, redirect, Blueprint
 import repositories.event_repository as event_repository
 
 events_blueprint = Blueprint("events", __name__)
+
+@events_blueprint.route("/events")
+def events():
+    events = event_repository.select_all()
+    return render_template("events/index.html", events = events)
--- controllers/player_controller.py
+++ controllers/player_controller.py
@@ -3,3 +3,14 @@ from flask import Blueprint, Flask, render_template, request, redirect
 import repositories.player_repository as player_repository
 
 players_blueprint = Blueprint("players", __name__)
+
+@players_blueprint.route("/players")
+def players():
+    players = player_repository.select_all()
+    return render_template("players/index.html", players = players)
+
+@players_blueprint.route("/players/<id>")
+def show(id):
+    player = player_repository.select(id)
+    events = player_repository.events(player)
+    return render_template("players/show.html", player = player, events = events)

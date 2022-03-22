8ad568be77242b42f3d834aefffa23329064ac68
Jacob <callaghanjacob@gmail.com>
2022-03-22 19:09:53 +0000

    add player functionality updated

--- controllers/player_controller.py
+++ controllers/player_controller.py
@@ -2,6 +2,7 @@ from flask import Blueprint, Flask, render_template, request, redirect
 
 import repositories.player_repository as player_repository
 import repositories.match_repository as match_repository
+from models.player import Player
 
 players_blueprint = Blueprint("players", __name__)
 
@@ -16,4 +17,15 @@ def show(id):
     matches = match_repository.select_all()
     return render_template("players/show.html", player = player, matches = matches)
 
-@players_blueprint.route("players/new")
+@players_blueprint.route("/players/new", methods=['GET'])
+def new_player():
+    return render_template("players/new.html")
+
+@players_blueprint.route("/players", methods=['POST'])
+def create_player():
+    name = request.form['name']
+    gender = request.form['gender']
+    weight_kg = request.form['weight_kg']
+    player = Player(name, gender, weight_kg)
+    player_repository.save(player)
+    return redirect("/players")
